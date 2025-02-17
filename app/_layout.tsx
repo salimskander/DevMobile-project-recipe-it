6cbc6f408e9b09b0693b2b1ecd53b5b4ca395d25
salim <salim.skander@ynov.com>
2025-02-17 11:03:10 +0100

    Initial commit
    Generated by create-expo-stack 2.12.2

---
+++ app/_layout.tsx
@@ -0,0 +1,19 @@
+import { ThemeProvider } from '@shopify/restyle';
+import { Stack } from 'expo-router';
+import { theme } from 'theme';
+
+export const unstable_settings = {
+  // Ensure that reloading on `/modal` keeps a back button present.
+  initialRouteName: '(tabs)',
+};
+
+export default function RootLayout() {
+  return (
+    <ThemeProvider theme={theme}>
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
+      </Stack>
+    </ThemeProvider>
+  );
+}
