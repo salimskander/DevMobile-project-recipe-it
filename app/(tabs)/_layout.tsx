0e908292b5f968592cab5f7bd3f32adb57b7b572
Elyter <eliottbreg@gmail.com>
2025-02-18 16:40:41 +0100

    delete tab two

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,49 +1,50 @@
 import { Stack, Tabs } from 'expo-router';
+
 import { TabBarIcon } from '../../components/TabBarIcon';
 
 export default function TabLayout() {
   return (
     <>
-    <Stack.Screen options={{ headerShown: false, gestureEnabled: false }} />
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: '#FF9F0A',
-        tabBarInactiveTintColor: '#9CA3AF',
-        tabBarShowLabel: false, // Cache les labels pour n'avoir que les icônes
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          headerShown: false,
-          tabBarIcon: ({ color }) => <TabBarIcon name="home" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="search"
-        options={{
-          title: 'Search',
-          headerShown: false,
-          tabBarIcon: ({ color }) => <TabBarIcon name="search" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="favorites"
-        options={{
-          title: 'Favorites',
-          headerShown: false,
-          tabBarIcon: ({ color }) => <TabBarIcon name="heart" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="profile"
-        options={{
-          title: 'Profile',
-          headerShown: false,
-          tabBarIcon: ({ color }) => <TabBarIcon name="user" color={color} />,
-        }}
-      />
-    </Tabs>
+      <Stack.Screen options={{ headerShown: false, gestureEnabled: false }} />
+      <Tabs
+        screenOptions={{
+          tabBarActiveTintColor: '#FF9F0A',
+          tabBarInactiveTintColor: '#9CA3AF',
+          tabBarShowLabel: false, // Cache les labels pour n'avoir que les icônes
+        }}>
+        <Tabs.Screen
+          name="index"
+          options={{
+            title: 'Home',
+            headerShown: false,
+            tabBarIcon: ({ color }) => <TabBarIcon name="home" color={color} />,
+          }}
+        />
+        <Tabs.Screen
+          name="search"
+          options={{
+            title: 'Search',
+            headerShown: false,
+            tabBarIcon: ({ color }) => <TabBarIcon name="search" color={color} />,
+          }}
+        />
+        <Tabs.Screen
+          name="favorites"
+          options={{
+            title: 'Favorites',
+            headerShown: false,
+            tabBarIcon: ({ color }) => <TabBarIcon name="heart" color={color} />,
+          }}
+        />
+        <Tabs.Screen
+          name="profile"
+          options={{
+            title: 'Profile',
+            headerShown: false,
+            tabBarIcon: ({ color }) => <TabBarIcon name="user" color={color} />,
+          }}
+        />
+      </Tabs>
     </>
   );
 }
